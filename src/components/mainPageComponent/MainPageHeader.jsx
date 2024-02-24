a6a2057f89d742219ddf92df1714eb12770ab310
t2023-m0037 <t2023-m0037@t2023-m0037ui-MacBookAir.local>
2024-02-24 19:10:40 +0900

    [style]헤더 뼈대 작업 완

--- src/components/mainPageComponent/MainPageHeader.jsx
+++ src/components/mainPageComponent/MainPageHeader.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import * as HS from '../mainPageComponent/mainPageHeaderStyle';
+import img from '../styles/image/logo.png';
 
 const MainPageHeader = () => {
-  return <div>MainPageHeader</div>;
+  const [isLogin, setIsLogin] = useState(true);
+  return (
+    <HS.Wrapper>
+      <HS.LogoImage src={img} />
+      {isLogin ? (
+        <HS.LoginStyle>
+          <HS.StBtn>커뮤니티</HS.StBtn>
+          <HS.AvatarStyle />
+          <HS.StBtn>로그아웃</HS.StBtn>
+        </HS.LoginStyle>
+      ) : (
+        <HS.StBtn>로그인</HS.StBtn>
+      )}
+    </HS.Wrapper>
+  );
 };
 
 export default MainPageHeader;
---
+++ src/components/mainPageComponent/mainPageHeaderStyle.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 85px;
+  border-bottom: 1px solid #eeeeee;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const LogoImage = styled.img`
+  width: 200px;
+  height: 70px;
+  margin-left: 15px;
+  cursor: pointer;
+`;
+export const StBtn = styled.button`
+  width: 100px;
+  height: 45px;
+  color: black;
+  background-color: #cae0d2;
+  border: 0px;
+  border-radius: 7px;
+  margin-right: 25px;
+  cursor: pointer;
+  &:hover {
+    transition: all 0.2s;
+    transform: scale(1.05);
+  }
+`;
+
+export const LoginStyle = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 7px;
+`;
+export const AvatarStyle = styled.img`
+  width: 60px;
+  height: 60px;
+  border: 1px solid black;
+  border-radius: 50%;
+  margin-right: 25px;
+`;
