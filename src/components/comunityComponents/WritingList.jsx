3020d13f0053b3575c01c24e57820c93c5efb991
DecidedCard <heajun35@gmail.com>
2024-02-24 16:40:23 +0900

    [feat] supabase data 불러오기

--- src/components/comunityComponents/WritingList.jsx
+++ src/components/comunityComponents/WritingList.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { getWriteList } from './supabaseTest';
 
 const WritingList = () => {
-  return <div>WritingList</div>;
+  const { isLoading, isError, data } = useQuery('comunityWriteList', getWriteList);
+
+  if (isLoading) {
+    return <div>로딩 중입니다</div>;
+  }
+  return <div>Writin</div>;
 };
 
 export default WritingList;
