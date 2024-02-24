36f521bf6f99b9cefb302300e853e1d849b2bc37
t2023-m0037 <t2023-m0037@t2023-m0037ui-MacBookAir.local>
2024-02-24 18:56:43 +0900

    [feat] Home컴포넌트에 MainPage imort

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import MainPage from 'components/mainPageComponent/MainPage';
+import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <MainPage />
+    </>
+  );
 };
 
 export default Home;
