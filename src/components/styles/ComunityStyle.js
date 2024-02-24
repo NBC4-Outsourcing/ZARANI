d53468b60405e5ed003077ae7fc5506ce297243d
DecidedCard <heajun35@gmail.com>
2024-02-24 15:14:51 +0900

    [style] imputform css 수정

--- src/components/styles/ComunityStyle.js
+++ src/components/styles/ComunityStyle.js
@@ -4,8 +4,11 @@ export const ComunityForm = styled.form`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  width: 100%;
+  width: 95%;
   height: 50px;
+  box-shadow: 0px 0px 3px 1px gray;
+  margin: 2.5%;
+  border-radius: 8px;
 `;
 
 export const ComunityInput = styled.input`
