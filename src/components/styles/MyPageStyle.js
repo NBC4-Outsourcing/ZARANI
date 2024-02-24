5822df6a57011338d3d61aab0303f6c5a07f9712
HaeRheeNam <4ran0318@gmail.com>
2024-02-24 18:00:36 +0900

    [Style] 이미지 css 크기 조정

---
+++ src/components/styles/MyPageStyle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ThumnailImg = styled.img`
+  min-width: 50px;
+  max-width: 240px;
+  width: 100%;
+  height: 100%;
+  min-height: 50px;
+  max-height: 240px;
+
+  object-fit: cover;
+`;
