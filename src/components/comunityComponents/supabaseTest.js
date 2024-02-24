810eb5491cfb24f46a0ca2b93c084c704acdc06d
DecidedCard <heajun35@gmail.com>
2024-02-24 15:48:37 +0900

    [feat] supabase api 작성

--- src/components/comunityComponents/supabaseTest.js
+++ src/components/comunityComponents/supabaseTest.js
@@ -2,4 +2,27 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://qyahoxqikvxujxdzlhsn.supabase.co';
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const insertWriting = async (newWrite) => {
+  const { data, error } = await supabase.from('comunityWrite').insert([newWrite]).select();
+  if (!error) {
+    alert('글을 저장하는데 성공했습니다.');
+    return data;
+  } else {
+    console.error(error);
+    alert('글을 저장하지 못했습니다.');
+  }
+};
+
+const getWrites = async () => {
+  const { data, error } = await supabase.from('comunityWrite').select('*');
+  if (!error) {
+    return data;
+  } else {
+    console.error(error);
+    alert('데이터를 가져오지 못 했습니다.');
+  }
+};
+
+export { insertWriting };
