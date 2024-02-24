58e3e38ed3acb57535dccab5651244ce49a99dd3
t2023-m0037 <t2023-m0037@t2023-m0037ui-MacBookAir.local>
2024-02-24 20:03:13 +0900

    [feat] 메인페이지 커뮤니티 섹션 컴포넌트 생성

--- src/components/mainPage/MainPageComunity.jsx
+++ src/components/mainPage/MainPageComunity.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import * as CS from '../styles/mainPageComunityStyle';
 
 const MainPageComunity = () => {
-  return <CS.Wrapper>MainPageComunity</CS.Wrapper>;
+  return <CS.Wrapper>커뮤니티</CS.Wrapper>;
 };
 
 export default MainPageComunity;
--- src/components/styles/mainPageComunityStyle.js
+++ src/components/styles/mainPageComunityStyle.js
@@ -3,5 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   border: 1px solid black;
   width: 500px;
-  height: 500px;
+  height: 250px;
+  margin: 30px 0 30px 50px;
 `;
