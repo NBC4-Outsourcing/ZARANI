dc9b5a3cfa105307de3f1c7fff61536bce3f4178
DecidedCard <heajun35@gmail.com>
2024-02-24 11:08:34 +0900

    [feat/#1] comunity branch 생성 및 기초 세팅

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,12 +1,14 @@
-import Home from "pages/Home";
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Comunity from 'pages/Comunity';
+import Home from 'pages/Home';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/comunity" element={<Comunity />} />
       </Routes>
     </BrowserRouter>
   );
