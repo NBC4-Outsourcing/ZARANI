ba33c90b3555ef64652e3a529eab0e02d27046f6
DecidedCard <heajun35@gmail.com>
2024-02-24 19:18:17 +0900

    [feat] comunityComponents/Comunity.jsx 파일 생성

---
+++ src/components/comunityComponents/ComunityMain.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WritingList from './WritingList';
+
+const ComunityMain = () => {
+  return (
+    <div>
+      <WritingList />
+    </div>
+  );
+};
+
+export default ComunityMain;
