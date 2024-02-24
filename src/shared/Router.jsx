bf6f92357865a5ecabf1d4f8c73626477ad4a697
HaeRheeNam <4ran0318@gmail.com>
2024-02-24 17:20:41 +0900

    [Feat] 마이 페이지 구현중

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -8,7 +8,7 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="mypage" element={<MyPage />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
