0cbfdbd48690c0065e67f8a6d8003e103efd2634
GNooNJo <jjjchoon@nate.com>
2024-02-25 16:35:25 +0900

    [feat/#19] - 로그인/회원가입 페이지 라우팅 설정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,6 +1,7 @@
 import Layout from 'components/layoutComponent/Layout';
 import MainPage from 'components/mainPageComponent/MainPage';
 import Comunity from 'pages/Comunity';
+import Login from 'pages/Login';
 import MyPage from 'pages/MyPage';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -13,6 +14,7 @@ const Router = () => {
           <Route path="/" element={<MainPage />} />
           <Route path="/comunity" element={<Comunity />} />
           <Route path="/mypage" element={<MyPage />} />
+          <Route path="/login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
