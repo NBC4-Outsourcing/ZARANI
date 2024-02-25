d3d4274aa8c00b4cc132fb8f21b380c553f644fa
HaeRheeNam <4ran0318@gmail.com>
2024-02-26 04:23:37 +0900

    [Feat/#46] 마이페이지 내가 쓴 후기 구현

--- src/components/myPageComponent/MyPageReviews.jsx
+++ src/components/myPageComponent/MyPageReviews.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import { readMyReview } from './myPageSupabase';
 import { getFormattedDate } from 'components/comunityComponents/formattedDate';
+import { supabase } from 'api/supabase/supabase';
 
 const MyPageReviews = () => {
-  const { isLoading, error, data: myReview } = useQuery('reviewWrite', readMyReview);
+  const [myReview, setMyReviews] = useState([]);
+  useEffect(() => {
+    const readMyReview = async () => {
+      const { data, error } = await supabase.from('reviewWrite').select('*');
+      let reviewArray = [];
+      data.forEach((readItem) => {
+        reviewArray.push(readItem);
+      });
+      setMyReviews(reviewArray);
+      if (error) {
+        alert('오류로 인해 정보를 받아오지 못 하고 있습니다.');
+      }
+    };
+    readMyReview();
+  }, []);
   console.log(myReview);
-  if (error) <div> 정보를 불러오지 못하고 있습니다.</div>;
+  // const [id, nickname, content, data, email] = myReview;
+  // if (error) <div> 정보를 불러오지 못하고 있습니다.</div>;
   return (
     <article>
       {myReview.map((reviews) => {
@@ -15,7 +31,7 @@ const MyPageReviews = () => {
           <section key={id}>
             <div>도로이름 : 안양천길 </div>
             <div>content: {content}</div>
-            <div>date : {new Date(data).toISOString()}</div>
+            <div>date : {data}</div>
           </section>
         );
       })}
