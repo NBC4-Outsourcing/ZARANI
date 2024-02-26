7987335557a486e61bc6a418e3bcb075be403ee2
DecidedCard <heajun35@gmail.com>
2024-02-26 20:53:35 +0900

    [feat] 에러 처리 작성

--- src/components/communityComponents/CommunitySupabase.js
+++ src/components/communityComponents/CommunitySupabase.js
@@ -2,23 +2,27 @@ import { supabase } from 'api/supabase/supabase';
 
 const getUser = async () => {
   const {
-    data: { user }
+    data: { user },
+    error: userData
   } = await supabase.auth.getUser();
+  if (userData) {
+    alert('로그인이 되어있지 않습니다.');
+  }
   return user;
 };
 
 const insertWriting = async (newWrite) => {
-  const { error } = await supabase.from('communityWrite').insert([newWrite]).select();
-  if (error) {
-    console.error(error);
-    alert('글을 저장하지 못했습니다.');
+  const { error: insertWrite } = await supabase.from('communityWrite').insert([newWrite]).select();
+  if (insertWrite) {
+    alert('글을 저장하지 못 했습니다.');
+    console.error(insertWrite);
   }
 };
 
 const getWriteList = async () => {
-  const { data, error } = await supabase.from('communityWrite').select('*');
-  if (error) {
-    console.error(error);
+  const { data, error: getWrite } = await supabase.from('communityWrite').select('*');
+  if (getWrite) {
+    console.error(getWrite);
     alert('데이터를 가져오지 못 했습니다.');
   } else {
     return data;
@@ -26,32 +30,33 @@ const getWriteList = async () => {
 };
 
 const updateWrite = async ([changeContent, id]) => {
-  const { error } = await supabase.from('communityWrite').update(changeContent).eq('id', id);
-  if (error) {
+  const { error: updateWrite } = await supabase.from('communityWrite').update(changeContent).eq('id', id);
+  if (updateWrite) {
     alert('변경하지 못 했습니다.');
-    console.error(error);
+    console.error(updateWrite);
   }
 };
 
 const deleteWrite = async (id) => {
-  const { error } = await supabase.from('communityWrite').delete().eq('id', id).select();
-  if (error) {
+  const { error: deleteWrite } = await supabase.from('communityWrite').delete().eq('id', id).select();
+  if (deleteWrite) {
     alert('삭제하지 못 했습니다.');
+    console.error(deleteWrite);
   }
 };
 
 const insertComment = async (newComment) => {
-  const { error } = await supabase.from('commentWrite').insert([newComment]).select();
-  if (error) {
-    console.error(error);
+  const { error: insertComment } = await supabase.from('commentWrite').insert([newComment]).select();
+  if (insertComment) {
+    console.error(insertComment);
     alert('댓글을 저장하지 못했습니다.');
   }
 };
 
 const getCommentList = async () => {
-  const { data, error } = await supabase.from('commentWrite').select('*');
-  if (error) {
-    console.error(error);
+  const { data, error: getComment } = await supabase.from('commentWrite').select('*');
+  if (getComment) {
+    console.error(getComment);
     alert('데이터를 가져오지 못 했습니다.');
   } else {
     return data;
@@ -59,9 +64,10 @@ const getCommentList = async () => {
 };
 
 const deleteComment = async (id) => {
-  const { error } = await supabase.from('commentWrite').delete().eq('id', id);
-  if (error) {
+  const { error: deleteComment } = await supabase.from('commentWrite').delete().eq('id', id);
+  if (deleteComment) {
     alert('삭제하지 못 했습니다.');
+    console.error(deleteComment);
   }
 };
 export { getUser, insertWriting, getWriteList, updateWrite, deleteWrite, insertComment, getCommentList, deleteComment };
--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -6,25 +6,21 @@ import { useQuery } from 'react-query';
 import { getCommentList, getUser, getWriteList } from 'components/communityComponents/CommunitySupabase';
 import Loading from 'components/common/Loading';
 import WritingList from 'components/communityComponents/WritingList';
+import { useNavigate } from 'react-router-dom';
 
 const Community = () => {
-  const { isLoading, isError, data: userData } = useQuery('userData', getUser);
-  const {
-    isLoading: writeListLoading,
-    isError: writeListError,
-    data: writeList
-  } = useQuery('communityWriteList', getWriteList);
-  const {
-    isLoading: commentListLoading,
-    isError: commentListError,
-    data: commentList
-  } = useQuery('commentWriteList', getCommentList);
+  const navigate = useNavigate();
+  const { isLoading, data: userData } = useQuery('userData', getUser);
+  const { isLoading: writeListLoading, data: writeList } = useQuery('communityWriteList', getWriteList);
+  const { isLoading: commentListLoading, data: commentList } = useQuery('commentWriteList', getCommentList);
 
   if (isLoading || writeListLoading || commentListLoading) {
     return <Loading />;
   }
-  if (isError || writeListError || commentListError) {
-    return <div>데이터를 가져오는 중 문제가 발생했습니다.</div>;
+
+  if (userData === null) {
+    navigate('/');
+    return <div>데이터를 가져오지 못했습니다.</div>;
   }
   return (
     <CommunityDiv>
