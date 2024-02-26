45e4e6b9452fecaf3366dbd30b9a3137031aa8d9
unknown <qpscm662@naver.com>
2024-02-27 01:17:54 +0900

    [feat]지도 검색 리스트 페이지 수 표시

--- src/components/mainPage/MainPageKaKao.jsx
+++ src/components/mainPage/MainPageKaKao.jsx
@@ -31,7 +31,7 @@ const MainPageKaKao = () => {
         displayPlaces(data);
 
         // 페이지 번호를 표출합니다
-        // displayPagination(pagination);
+        displayPagination(pagination);
       } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
         alert('검색 결과가 존재하지 않습니다.');
         return;
@@ -127,6 +127,36 @@ const MainPageKaKao = () => {
       el.className = 'item';
 
       return el;
+    }
+    // 검색결과 목록 하단에 페이지번호를 표시는 함수입니다
+    function displayPagination(pagination) {
+      var paginationEl = document.getElementById('pagination'),
+        fragment = document.createDocumentFragment(),
+        i;
+
+      // 기존에 추가된 페이지번호를 삭제합니다
+      while (paginationEl.hasChildNodes()) {
+        paginationEl.removeChild(paginationEl.lastChild);
+      }
+
+      for (i = 1; i <= pagination.last; i++) {
+        var el = document.createElement('a');
+        el.href = '#';
+        el.innerHTML = i;
+
+        if (i === pagination.current) {
+          el.className = 'on';
+        } else {
+          el.onclick = (function (i) {
+            return function () {
+              pagination.gotoPage(i);
+            };
+          })(i);
+        }
+
+        fragment.appendChild(el);
+      }
+      paginationEl.appendChild(fragment);
     }
 
     // 마커를 생성하고 지도 위에 마커를 표시하는 함수입니다
