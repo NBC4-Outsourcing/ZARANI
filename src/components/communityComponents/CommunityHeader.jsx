461ee029a5671ed9628841332c4b365637c38185
DecidedCard <heajun35@gmail.com>
2024-02-26 19:26:40 +0900

    [feat] Community Header user정보 연동

--- src/components/communityComponents/CommunityHeader.jsx
+++ src/components/communityComponents/CommunityHeader.jsx
@@ -8,9 +8,19 @@ import {
 import React from 'react';
 import defaultImage from 'assets/defaultImage.png';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logout } from 'shared/redux/modules/authSlice';
 
-const CommunityHeader = () => {
+const CommunityHeader = ({ data }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { avatar } = data.user_metadata;
+
+  const onClickLogOutHandler = () => {
+    dispatch(logout());
+    alert('로그아웃 되었습니다');
+    navigate('/');
+  };
   return (
     <CommunityHeaderMain>
       <div>
@@ -18,8 +28,10 @@ const CommunityHeader = () => {
       </div>
       <CommunityHeaderName>ZARANI COMMUNITY</CommunityHeaderName>
       <CommunityHeaderInfo>
-        <CommunityHeaderImage src={defaultImage} />
-        <CommunityBtn background={'danger'}>로그아웃</CommunityBtn>
+        <CommunityHeaderImage src={avatar ? avatar : defaultImage} />
+        <CommunityBtn background={'danger'} onClick={onClickLogOutHandler}>
+          로그아웃
+        </CommunityBtn>
       </CommunityHeaderInfo>
     </CommunityHeaderMain>
   );
--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -5,11 +5,20 @@ import { CommunityDiv } from 'components/styles/CommunityStyle';
 import CommunityHeader from 'components/communityComponents/CommunityHeader';
 import { useQuery } from 'react-query';
 import { getUser } from 'components/communityComponents/CommunitySupabase';
+import Loading from 'components/common/Loading';
 
 const Community = () => {
+  const { isLoading, isError, data } = useQuery('userData', getUser);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (isError) {
+    return <div>데이터를 가져오는 중 문제가 발생했습니다.</div>;
+  }
   return (
     <CommunityDiv>
-      <CommunityHeader />
+      <CommunityHeader data={data} />
       <CommunityInputForm />
       <CommunityMain />
     </CommunityDiv>
