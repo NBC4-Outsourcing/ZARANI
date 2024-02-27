c25be0209d61a3fbbec183a82653405d21d8ba44
HaeRheeNam <4ran0318@gmail.com>
2024-02-27 09:12:18 +0900

    [Feat/#72] MyPage 로그인 상태 기준 페이지 접근 설정

--- src/components/myPageComponent/myPageSupabase.js
+++ src/components/myPageComponent/myPageSupabase.js
@@ -21,7 +21,6 @@ export const readUserAccount = async () => {
 
 export const readUserInfo = async () => {
   const { data, error } = await supabase.from('usersAccounts').select('*');
-  // const [spreadData] = data;
   if (error) {
     alert('오류로 인해 정보를 받아오지 못 하고 있습니다.');
   }
