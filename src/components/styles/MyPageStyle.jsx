49c0e7fdd4f24eda89358b6c9f337fcb186cbcb2
HaeRheeNam <4ran0318@gmail.com>
2024-02-28 03:22:56 +0900

    [Style/#92] contents css 완료

--- src/components/styles/MyPageStyle.jsx
+++ src/components/styles/MyPageStyle.jsx
@@ -9,6 +9,42 @@ export const MyPageContainerSection = styled.section`
   justify-content: center;
 `;
 
+export const UseMyStreetDosDiv = styled.div`
+  width: 100%;
+  min-width: 300px;
+  max-width: 1280px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 1rem;
+  gap: 2rem;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const MyStreetsDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  padding: 0.8rem;
+
+  border-radius: 1rem;
+  background-color: rgba(250, 184, 145, 0.8);
+`;
+
+export const MyStreetP = styled.p`
+  font-size: 1.4rem;
+  font-weight: bold;
+  padding: 0.4rem 1rem;
+`;
+
 export const ContentsReviewArticle = styled.article`
   width: 100%;
   min-width: 300px;
@@ -21,23 +57,8 @@ export const ContentsReviewArticle = styled.article`
   justify-content: center;
   padding: 1rem;
   gap: 1rem;
-  /* width: 100%;
-  min-width: 300px;
-  max-width: 1280px;
 
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-
-  margin-top: 1.2rem; */
   @media screen and (max-width: 768px) {
-    /* 화면 너비가 768px 이하일 때 세로로 배치 */
     flex-direction: column;
   }
-
-  /* align-items: center;
-  justify-content: center;
-  padding: 1rem;
-
-  gap: 1rem; */
 `;
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,18 +1,26 @@
+import { useQuery } from 'react-query';
+import { readMyReview } from 'components/myPageComponent/myPageSupabase';
 import MyPageReviews from 'components/myPageComponent/MyPageReviews';
 import MyPageContents from 'components/myPageComponent/MyPageContents';
 import MyPageHeader from 'components/myPageComponent/MyPageHeader';
 import * as MP from 'components/styles/MyPageStyle';
-import { useQuery } from 'react-query';
-import { readMyReview } from 'components/myPageComponent/myPageSupabase';
 
 const MyPage = () => {
   const { data: myReview, error, isLoading } = useQuery('myReviews', readMyReview);
   return (
     <MP.MyPageContainerSection>
       <MyPageHeader />
+
       <MP.ContentsReviewArticle>
         <MyPageContents />
-        <MyPageReviews myReview={myReview} isLoading={isLoading} />
+        <MP.UseMyStreetDosDiv>
+          <MP.MyStreetsDiv>
+            <MP.MyStreetP>
+              <span>내가 이용한 자전거 도로 후기들</span>
+            </MP.MyStreetP>
+          </MP.MyStreetsDiv>
+          <MyPageReviews myReview={myReview} isLoading={isLoading} />
+        </MP.UseMyStreetDosDiv>
       </MP.ContentsReviewArticle>
     </MP.MyPageContainerSection>
   );
