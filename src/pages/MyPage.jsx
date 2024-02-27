c36a5a00f07259e773a047c465a57f56960d3553
HaeRheeNam <4ran0318@gmail.com>
2024-02-27 10:32:52 +0900

    [Feat/#74]마이 페이지 id 에러처리 및 리액트 쿼리로 다시 리팩토링 중

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -8,21 +8,13 @@ import MyPageHeader from 'components/myPageComponent/MyPageHeader';
 import Loading from 'components/common/Loading';
 
 const MyPage = () => {
-  const data = {
-    id: '1',
-    nickname: '보라돌이',
-    email: '1234@asdf.com',
-    avatar:
-      'https://rtjzvtuqyafegkvoirwc.supabase.co/storage/v1/object/public/unAuthUserImage/userOneImage/1d25d250-5dea-47f5-a465-05f74a7bd79d',
-    uid: 'qeqr123qwfs3'
-  };
   // const { isLoading, isError, data } = useQuery('usersAccounts', readUserInfo);
   // // useQuery => refetching
   // if (isError) return <div>오류로 인해 정보를 받아오지 못 하고 있습니다.</div>;
   return (
     <section>
       <MyPageHeader />
-      <MyPageContents data={data} />
+      <MyPageContents />
       {/* <MyPageContents data={data} isLoading={isLoading} /> */}
       <MyPageReviews />
     </section>
