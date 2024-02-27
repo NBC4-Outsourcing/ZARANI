7a7796acc6ddaaef34e91c13185bc3a0d6636f14
서가희 <kahee430@gmail.com>
2024-02-27 12:00:31 +0900

    [fix] 리뷰 이미지 등록 수정

--- src/components/reviewComponents/ReviewList.jsx
+++ src/components/reviewComponents/ReviewList.jsx
@@ -1,104 +1,71 @@
 import { supabase } from 'api/supabase/supabase';
 import { getFormattedDate } from 'components/communityComponents/formattedDate';
 import { ContentsList } from 'components/styles/ReviewStyle';
-import React, { useEffect, useState } from 'react';
-import { useQuery } from 'react-query';
-import { getLocalStorageJSON } from 'utils/getLocalStorageJSON';
+import { useEffect, useState } from 'react';
 
 export const ReviewList = () => {
-  const result = getLocalStorageJSON();
-  console.log('result', result);
-
   // 데이터베이스에 저장된 데이터 저장 state
-  const [reviewData, setReviewData] = useState();
-  const [reviewImg, setReviewImg] = useState();
+  const [reviewData, setReviewData] = useState([]);
 
   // 수정 여부 state
   const [isEditing, setIsEditing] = useState(false);
 
-  // usersAccounts data state
-  const [userEmail, setUserEmail] = useState([{}]);
-  // userInfo
-  useEffect(() => {
-    const readUserInfo = async () => {
-      const { data, error } = await supabase.from('usersAccounts').select('*');
-      setUserEmail(data);
-      // console.log('userInfoData', data);
-
-      if (error) {
-        alert('오류로 인해 정보를 받아오지 못 하고 있습니다.');
-        return null;
-      }
-    };
-
-    readUserInfo();
-  }, []);
-  const [userInfo] = userEmail;
-  const { email } = userInfo;
-  console.log('email', email);
-  // 여기까지usersAccounts data state
-
-  // DB에 저장된 후기 데이터 가져오기
+  // DB에 저장된 데이터 가져오기
   useEffect(() => {
     // 게시글 불러오기
     const fetchData = async () => {
-      try {
-        let { data: reviewWrite, error } = await supabase.from('reviewWrite').select('*');
-        if (!error) {
-          setReviewData(reviewWrite);
-        }
-      } catch (error) {
-        console.log('reviewDataError', error);
-      }
-    };
-
-    // 이미지 불러오기
-    // usersAccounts테이블의 uid를 정상적으로 받아오지못해 임시로 파일명 지정 나중에 회원 정보 기능이 완성되면 회원 uid를 파일 이름에 넣어줄것
-    const filePath = async () => {
-      // const { data: imageUrl, error } = supabase.storage.from('reviewImage').getPublicUrl(`reviewFile/${uid}`);
-      const { data: imageUrl, error } = supabase.storage.from('reviewImage').getPublicUrl('reviewFile/testImg');
-      if (!error) {
-        console.log('imageUrlData', imageUrl);
-        console.log('이미지 Url 변환 성공', imageUrl);
-        setReviewImg(imageUrl.publicUrl);
-        console.log('reviewImg', reviewImg);
+      let { data: reviewWrite, error } = await supabase.from('reviewWrite').select('*');
+      console.log('reviewWrite', reviewWrite);
+      if (error) {
+        console.log('게시물 조회 실패', error);
       } else {
-        console.log('imageUrlDataError', error);
+        console.log('게시물 조회 성공', reviewWrite);
+        setReviewData(reviewWrite);
       }
     };
-    console.log('reviewImg', reviewImg);
 
     fetchData();
+  }, []);
+
+  useEffect(() => {
+    // // 이미지 목록 조회?
+    const filePath = async () => {
+      try {
+        const { data, error } = await supabase.storage.from('reviewImage').list('reviewFile/files', {
+          limit: 100, // 가져올 파일 개수 제한 (최대 100개)
+          offset: 0 // 가져올 파일의 오프셋
+          // sortBy: { column: 'name', order: 'asc' }, // 파일 이름을 기준으로 정렬할 경우 추가
+        });
+
+        if (error) {
+          console.log('이미지 조회 실패', error);
+        }
+        console.log('이미지 조회 성공', data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
     filePath();
   }, []);
 
-  // 회원 정보가 없어 임시로 usersAccounts table email 사용, 회원 정보 완료 시 회원 email 또는 uid 활용할것
   // 데이터 삭제
-  const removeReview = async (email) => {
+  const removeReview = async (email, reviewimg) => {
     if (window.confirm('게시물을 삭제하시겠습니까?')) {
-      // 이미지 삭제
       try {
-        const { data, error } = await supabase.storage.from('reviewImage').remove('reviewFile/testImg');
+        const { data, error } = await supabase.storage.from('reviewImage').remove(`reviewFile/files/`);
         if (!error) {
-          console.log('removeReviewData', data);
-          console.log('이미지 삭제 성공');
+          console.log('이미지 삭제 성공', data);
         }
       } catch (error) {
         console.log('이미지 삭제 실패', error);
       }
-
       // 게시글 삭제
       // 선택한 게시물만 삭제하도록 수정
       try {
-        const { error } = await supabase.from('reviewWrite').delete().eq('email', email);
+        const { error } = await supabase.from('reviewWrite').delete().eq('iemaild', email);
         if (!error) {
           alert('게시물이 삭제되었습니다.');
-          console.log('error', error);
-
-          setReviewData((prevData) => prevData.filter((item) => item.email !== email));
-
-          // 이미지도 선택한 이미지와 이름이 같으면 삭제되도록
-          setReviewImg(null);
+          console.log('게시물 삭제 성공', error);
           return;
         }
       } catch (error) {
@@ -108,26 +75,28 @@ export const ReviewList = () => {
   };
 
   // 데이터 수정
-  const modifyReview = () => {};
+  // const modifyReview = () => {};
+
   return (
     <ContentsList>
-      {reviewData?.map((item, idx) => (
-        <div key={idx}>
-          {console.log('reviewImg', reviewImg)}
-          {item.reviewimg && <img src={reviewImg} alt="리뷰 이미지" />}
-          <div>{item.nickname}</div>
-          <div>{item.content}</div>
-          <div>{getFormattedDate(item.date)}</div>
-          <button>수정</button>
-          <button
-            onClick={() => {
-              removeReview(item.email);
-            }}
-          >
-            삭제
-          </button>
-        </div>
-      ))}
+      {reviewData?.map((item) => {
+        return (
+          <div key={item.id}>
+            {item.reviewimg && <img src={item.reviewimg} alt="리뷰 이미지" />}
+            <div>{item.nickname}</div>
+            <div>{item.content}</div>
+            <div>{getFormattedDate(item.date)}</div>
+            <button>수정</button>
+            <button
+              onClick={() => {
+                removeReview(item.email, item.reviewimg);
+              }}
+            >
+              삭제
+            </button>
+          </div>
+        );
+      })}
     </ContentsList>
   );
 };
