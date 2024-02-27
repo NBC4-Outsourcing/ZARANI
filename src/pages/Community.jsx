65361f7e6cac1551994855d056b3dabd9da8f08e
DecidedCard <heajun35@gmail.com>
2024-02-27 11:52:53 +0900

    [feat] 에러 처리 세분화

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,6 +1,6 @@
 import CommunityInputForm from 'components/communityComponents/CommunityInputForm';
 import React from 'react';
-import { CommunityDiv } from 'components/styles/CommunityStyle';
+import { CommunityBtn, CommunityDiv, CommunityErrorBackground } from 'components/styles/CommunityStyle';
 import CommunityHeader from 'components/communityComponents/CommunityHeader';
 import { useQuery } from 'react-query';
 import { getCommentList, getUser, getWriteList } from 'components/communityComponents/CommunitySupabase';
@@ -20,12 +20,25 @@ const Community = () => {
 
   if (userData.__isAuthError && !(isLoading || writeListLoading || commentListLoading)) {
     return (
-      <div>
-        <p>로그인이 되어있지 않습니다.</p> <button onClick={() => navigate('/')}>홈으로</button>
-      </div>
+      <CommunityErrorBackground>
+        <p>로그인이 되어있지 않습니다.</p> <CommunityBtn onClick={() => navigate('/')}>홈으로</CommunityBtn>
+      </CommunityErrorBackground>
     );
   }
-
+  if (writeList.code && !(isLoading || writeListLoading || commentListLoading)) {
+    return (
+      <CommunityErrorBackground>
+        <p>글을 불러오지 못 했습니다.</p> <CommunityBtn onClick={() => navigate('/')}>홈으로</CommunityBtn>
+      </CommunityErrorBackground>
+    );
+  }
+  if (commentList.code && !(isLoading || writeListLoading || commentListLoading)) {
+    return (
+      <CommunityErrorBackground>
+        <p>댓글을 불러오지 못 했습니다.</p> <CommunityBtn onClick={() => navigate('/')}>홈으로</CommunityBtn>
+      </CommunityErrorBackground>
+    );
+  }
   return (
     <CommunityDiv>
       <CommunityHeader userData={userData} />
