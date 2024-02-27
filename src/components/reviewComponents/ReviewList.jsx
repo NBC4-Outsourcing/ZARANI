292890efc7b0d502b74e41bbb17f6d3ff78fe8ad
서가희 <kahee430@gmail.com>
2024-02-27 18:54:40 +0900

    [feat #67] 단일 데이터 삭제 기능 구현

--- src/components/reviewComponents/ReviewList.jsx
+++ src/components/reviewComponents/ReviewList.jsx
@@ -2,13 +2,14 @@ import { supabase } from 'api/supabase/supabase';
 import { getFormattedDate } from 'components/communityComponents/formattedDate';
 import { ContentsList } from 'components/styles/ReviewStyle';
 import { useEffect, useState } from 'react';
+import { ReviewUpdateForm } from './ReviewUpdateForm';
 
 export const ReviewList = () => {
   // 데이터베이스에 저장된 데이터 저장 state
   const [reviewData, setReviewData] = useState([]);
 
   // 수정 여부 state
-  const [isEditing, setIsEditing] = useState(false);
+  const [editDataId, setEditDataId] = useState('false');
 
   // DB에 저장된 데이터 가져오기
   useEffect(() => {
@@ -21,6 +22,7 @@ export const ReviewList = () => {
       } else {
         console.log('게시물 조회 성공', reviewWrite);
 
+        // 이미지 불러오기
         const reviewsWriteData = reviewWrite.map((item) => {
           const imgUrl = supabase.storage.from('reviewImage').getPublicUrl(item.reviewimg);
           return { ...item, imageUrl: imgUrl.data.publicUrl };
@@ -59,29 +61,41 @@ export const ReviewList = () => {
       }
     }
   };
-
-  // 데이터 수정
-  // const modifyReview = () => {};
+  console.log('reviewData', reviewData);
   return (
     <ContentsList>
-      {reviewData?.map((item) => {
-        return (
-          <div key={item.id}>
-            {item.reviewimg && <img src={item.imageUrl} alt="리뷰 이미지" />}
-            <div>{item.nickname}</div>
-            <div>{item.content}</div>
-            <div>{getFormattedDate(item.date)}</div>
-            <button>수정</button>
-            <button
-              onClick={() => {
-                removeReview(item.id, item.reviewimg);
-              }}
-            >
-              삭제
-            </button>
-          </div>
-        );
-      })}
+      {reviewData?.map((item) => (
+        <div key={item.id}>
+          {/* 수정 버튼 클릭 시 editDataId state에 item.id가 담겨 선택한 게시물만 수정 상태로 만들어 준다 */}
+          {editDataId === item.id ? (
+            <>
+              <ReviewUpdateForm item={item} />
+              <button onClick={setEditDataId(null)}>취소</button>
+            </>
+          ) : (
+            <div>
+              {item.imageUrl && <img src={item.imageUrl} alt="리뷰 이미지" />}
+              <div>{item.nickname}</div>
+              <div>{item.content}</div>
+              <div>{getFormattedDate(item.date)}</div>
+              <button
+                onClick={() => {
+                  setEditDataId(item.id);
+                }}
+              >
+                수정
+              </button>
+              <button
+                onClick={() => {
+                  removeReview(item.id);
+                }}
+              >
+                삭제
+              </button>
+            </div>
+          )}
+        </div>
+      ))}
     </ContentsList>
   );
 };
---
+++ src/components/reviewComponents/ReviewUpdateForm.jsx
@@ -0,0 +1,103 @@
+import { ContentsList } from 'components/styles/ReviewStyle';
+import React, { useRef, useState } from 'react';
+import useInput from '../../hooks/useInput';
+import { supabase } from 'api/supabase/supabase';
+
+export const ReviewUpdateForm = ({ item }) => {
+  const imgRef = useRef(null);
+  const [editData, setEditData] = useState([item]);
+  const [editImg, setEditImg] = useState('');
+  const [updateInput, onUpdateContentHandler] = useInput({
+    updateContent: ''
+  });
+  const { updateContent } = updateInput;
+
+  // 이미지 미리보기
+  const editImgHandler = (e) => {
+    const imgFile = e.target.files[0];
+    if (imgFile) {
+      const imgUrl = URL.createObjectURL(imgFile);
+      setEditImg(imgUrl);
+    } else {
+      console.log('이미지 파일이 선택되지 않았습니다');
+    }
+  };
+
+  // 데이터 수정
+
+  const modifyReview = async (id, reviewimg) => {
+    // 이미지 수정
+    try {
+      const imgPath = imgRef.current.files[0];
+      const imgUpdate = await supabase.storage.from('reviewImage').upload(reviewimg, imgPath, {
+        upsert: true
+      });
+      console.log('이미지 수정 완료', imgUpdate);
+    } catch (error) {
+      console.log('이미지 수정 실패', error);
+    }
+    // 데이터 수정
+    try {
+      const modifyReviewData = {
+        ...editData,
+        // marker,
+        // email,
+        // nickname,
+        // avatar,
+        content: updateContent,
+        reviewimg: '이미지 수정 완료 data path'
+      };
+      const { data, error } = await supabase.from('reviewWrite').update({ id, modifyReviewData }).eq('id', id).select();
+      if (!error) {
+        console.log('게시물 수정 완료', data);
+      }
+    } catch (error) {
+      console.log('게시물 수정 실패'), error;
+    }
+  };
+  console.log('editData', editData);
+  return (
+    <div>
+      {editData.map((item) => {
+        return (
+          <div key={item.id}>
+            <label>
+              {item.imageUrl && <img src={editImg ? editImg : item.imageUrl} alt="이미지" />}
+              <p>이미지 수정 시 이미지를 클릭해 주세요</p>
+              <input ref={imgRef} onChange={editImgHandler} type="file" accept="image/*" />
+            </label>
+            <div>{item.nickname}</div>
+            <textarea name="updateContent" value={updateContent} onChange={onUpdateContentHandler}>
+              {item.content}
+            </textarea>
+            <button
+              onClick={() => {
+                modifyReview(item.id, item.reviewimg);
+              }}
+            >
+              수정완료
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+// 수정 전용 컴포넌트 생성
+// 삼항연산자부분에 수정 컴포넌트를 연결 아이템을 프롭으로 전달
+
+// 수정 컴포넌트에서
+
+// 프롭으로 받은 데이터를 스테이트를 만들어서 저장 (이전데이터값)
+
+// 데이터를 수정하고 수정 완료버튼클릭
+
+// 등록일은 수정이 안되니까...
+// 수정데이터 저장된 스테이트
+// new{
+
+// }
+
+// 수정 완료를 누르면 이전 데이터 값을 저장한 스테이트에 셋을 이용해서 데이터를 다시 저장
+// 변화가 없는 데이터는 이전 값을 가져와서 사용하고 content
