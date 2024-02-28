32049e1ec6741047620372a7a04202ffe307a60e
서가희 <kahee430@gmail.com>
2024-02-28 21:52:31 +0900

    [style] 리뷰 페이지 스타일 수정

--- src/components/reviewComponents/ReviewHeader.jsx
+++ src/components/reviewComponents/ReviewHeader.jsx
@@ -1,16 +1,33 @@
 import { ReviewHeaderDiv } from 'components/styles/ReviewStyle';
-import * as MPH from 'components/styles/MyPageHeaderStyle';
 import * as RV from 'components/styles/ReviewStyle';
+import { logout } from 'shared/redux/modules/authSlice';
+import { useDispatch } from 'react-redux';
+import * as MPH from 'components/styles/MyPageHeaderStyle';
+import { getLoginUserInfo, removeCurrentLoginUser } from 'components/loginPageComponents/loginPageSupabase';
 
 const ReviewHeader = ({ placename }) => {
   const headerName = placename.placename;
+
+  const dispatch = useDispatch();
+  const handLogOut = async () => {
+    // 현재 로그인 중인 유저의 정보를 가져옴
+    const currentLoginUser = await getLoginUserInfo();
+    // 현재 로그인 중인 유저의 id(=uid)를 가져옴
+    const { id } = currentLoginUser.user;
+    // 현재 로그인 상태인 유저 DB에서 현재 유저를 삭제
+    await removeCurrentLoginUser(id);
+    dispatch(logout());
+    alert('로그아웃 되었습니다');
+  };
   return (
     <>
       <ReviewHeaderDiv>
         <MPH.HeaderNav>
           <MPH.HomeLink to="/">홈으로</MPH.HomeLink>
-          <RV.Title>{<h1>{headerName}</h1>}</RV.Title>
-          <div style={{ fontSize: '1.2rem' }}>자전거 도로 후기</div>
+          <RV.Title>
+            <h1>{headerName}</h1>
+          </RV.Title>
+          <MPH.LogoutBtn onClick={handLogOut}>로그아웃</MPH.LogoutBtn>
         </MPH.HeaderNav>
       </ReviewHeaderDiv>
     </>
